76f7e7625f180aa196a0a838f6848c5f9475f9b4
Rybers922 <francimar_456@hotmail.com>
2024-06-10 19:43:32 +0000

    Estruturas-de-dados

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,14 @@
 # Crie uma lista apenas com elementos numéricos
-
+numerica = [1, 2, 3, 4, 5]
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
-
+mista = ['python', 4,[1,2,3,4, True]]
 # Imprima na tela apenas os 5 primeiros elementos da lista
-
+mista [0:5:3]
 # Crie um slice na lista para que imprima na tela os elementos de índice par
-
+numerica[0:5:2]
 # Remova da lista o último item
-
+numerica.pop
 # Insira na lista um novo item
-
+numerica.append(6)
 # Remova da lista um item específico
+numerica.remove(1)
