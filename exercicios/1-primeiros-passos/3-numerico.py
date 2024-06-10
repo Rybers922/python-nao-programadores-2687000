861dc3f7614e7532f42201f22b0f5938b8baf16d
Rybers922 <francimar_456@hotmail.com>
2024-06-10 16:47:26 +0000

    Numericos

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -4,8 +4,17 @@ altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
 
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
 
+f'Hoje eu estudei {10 + idade_numerica} minutos'
+
 
 # Realize uma operação entre dados do tipo inteiro e float
+
+resultado = idade_numerica + altura
+
+
